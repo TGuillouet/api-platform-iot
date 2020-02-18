2b5c4f01de800478f49388f5c20d641a3ab67dba
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-18 14:59:33 +0100

    Essai de config

--- socket/server.js
+++ socket/server.js
@@ -2,104 +2,102 @@ const io = require('socket.io')();
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
+const SERIAL_PORT_NAME = '/dev/tty.SLAB_USBtoUART1';
 
 var xbeeAPI = new xbee_api.XBeeAPI({
-  api_mode: 2
+	api_mode: 2
 });
 
-
-let serialport = new SerialPort("/dev/ttyUSB1", {
-  baudRate: 9600,
-}, function (err) {
-  if (err) {
-    return console.log('Error: ', err.message)
-  }
-});
+let serialport = new SerialPort(
+	SERIAL_PORT_NAME,
+	{
+		baudRate: 9600
+	},
+	function(err) {
+		if (err) {
+			return console.log('Error: ', err.message);
+		}
+	}
+);
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
-serialport.on("open", function () {
-  var frame_obj = { // AT Request to be sent
-    type: C.FRAME_TYPE.AT_COMMAND,
-    command: "NI",
-    commandParameter: [],
-  };
+serialport.on('open', function() {
+	var frame_obj = {
+		// AT Request to be sent
+		type: C.FRAME_TYPE.AT_COMMAND,
+		command: 'NI',
+		commandParameter: []
+	};
 
-  xbeeAPI.builder.write(frame_obj);
+	xbeeAPI.builder.write(frame_obj);
 
-  frame_obj = { // AT Request to be sent
-    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
-    destination64: "FFFFFFFFFFFFFFFF",
-    command: "NI",
-    commandParameter: [],
-  };
-  xbeeAPI.builder.write(frame_obj);
-
+	frame_obj = {
+		// AT Request to be sent
+		type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+		destination64: 'FFFFFFFFFFFFFFFF',
+		command: 'NI',
+		commandParameter: []
+	};
+	xbeeAPI.builder.write(frame_obj);
 });
 
 // All frames parsed by the XBee will be emitted here
 
-xbeeAPI.parser.on("data", function (frame) {
+xbeeAPI.parser.on('data', function(frame) {
+	//on new device is joined, register it
 
-  //on new device is joined, register it
+	//on packet received, dispatch event
+	//let dataReceived = String.fromCharCode.apply(null, frame.data);
+	if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
+		console.log('C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET');
+		let dataReceived = String.fromCharCode.apply(null, frame.data);
+		console.log('>> ZIGBEE_RECEIVE_PACKET >', dataReceived);
 
-  //on packet received, dispatch event
-  //let dataReceived = String.fromCharCode.apply(null, frame.data);
-  if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
-    console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
-    let dataReceived = String.fromCharCode.apply(null, frame.data);
-    console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+		browserClient &&
+			browserClient.emit('pad-event', {
+				device: frame.remote64,
+				data: dataReceived
+			});
+	}
 
-    browserClient && browserClient.emit('pad-event', {
-      device: frame.remote64,
-      data: dataReceived
-    });
-  }
-
-  if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
-    // console.log(">> ZIGBEE_RECEIVE_PACKET >", frame);
-
-
-  } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-
-
-
-  } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    
-  } else {
-    console.debug(frame);
-    let dataReceived = String.fromCharCode.apply(null, frame.commandData)
-    console.log(dataReceived);
-  }
-
+	if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
+		// let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
+		// console.log(">> ZIGBEE_RECEIVE_PACKET >", frame);
+	} else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+	} else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
+	} else {
+		console.debug('Frame: ', frame);
+		let dataReceived = String.fromCharCode.apply(null, frame.commandData);
+		console.log(dataReceived);
+	}
 });
 let browserClient;
 io.on('connection', (client) => {
-  console.log(client.client.id);
-  browserClient = client;
+	console.log(client.client.id);
+	browserClient = client;
 
-  client.on('subscribeToPad', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-    // setInterval(() => {
-    //   client.emit('pad-event', {
-    //     device: "test device",
-    //     data: Math.round(Math.random()) * 2 - 1
-    //   })
-    //   ;
-    // }, Math.random() * 1000);
-  });
+	client.on('subscribeToPad', (interval) => {
+		console.log('client is subscribing to timer with interval ', interval);
+		// setInterval(() => {
+		//   client.emit('pad-event', {
+		//     device: "test device",
+		//     data: Math.round(Math.random()) * 2 - 1
+		//   })
+		//   ;
+		// }, Math.random() * 1000);
+	});
 
-  client.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
+	client.on('disconnect', () => {
+		console.log('Client disconnected');
+	});
 });
 
 const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
-//
+
 // serial_xbee.on("data", function(data) {
 //     console.log(data.type);
 //   // console.log('xbee data received:', data.type);
@@ -116,7 +114,7 @@ console.log('listening on port ', port);
 //       console.log(err);
 //   });
 // });
-//
+
 // shepherd.start(function (err) {                // start the server
 //   if (err)
 //     console.log(err);
