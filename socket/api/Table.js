07728eafaa8d224deecb38c7cfcd001ef81d3f6c
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-20 16:16:14 +0100

    Modifying the response management

--- socket/api/Table.js
+++ socket/api/Table.js
@@ -1,4 +1,5 @@
 const request = require('./../helpers/request');
+const ChairsModel = require('./Chair');
 
 /**
  * Get all tables
@@ -8,6 +9,15 @@ async function getTables() {
 	return await request('/r_tables', 'GET');
 }
 
+/**
+ * Update a table
+ * @return {Promise<[Table]>} The list of all chairs for a table
+ */
+async function updateTable(id, updates) {
+	return await request(`/r_tables/${id}`, 'PATCH', updates, { 'Content-Type': 'application/merge-patch+json' });
+}
+
 module.exports = {
-	getTables
+	getTables,
+	updateTable
 };
